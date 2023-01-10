4a204786c53da617c7465a5f8e662c3a0dfda5e3
Ruslan Tendora <ruslantendora@gmail.com>
2023-01-10 22:48:56 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
-
 import { Modal } from './Modal/Modal';
 import css from './App.module.css';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    searchName: '',
-    shovModal: false,
-    modalImg: '',
+export const App = () => {
+  const [searchName, setSearchName] = useState('');
+  const [shovModal, setShovModal] = useState(false);
+  const [modalImg, setModalImg] = useState('');
+
+  const hendlerFormSubmit = name => {
+    setSearchName(name);
   };
 
-  hendlerFormSubmit = name => {
-    this.setState({ searchName: name });
+  const toggleModal = () => {
+    setShovModal(state => !state);
   };
 
-  toggleModal = () => {
-    this.setState(prevState => {
-      return { shovModal: !prevState.shovModal };
-    });
+  const openModal = e => {
+    toggleModal();
+    setModalImg({ src: e.target.src, alt: e.target.alt });
   };
 
-  openModal = e => {
-    this.toggleModal();
-    this.setState({ modalImg: { src: e.target.src, alt: e.target.alt } });
-  };
-
-  render() {
-    const { searchName, modalImg, shovModal } = this.state;
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.hendlerFormSubmit} />
-        <ImageGallery searchName={searchName} openModal={this.openModal} />
-        {shovModal && (
-          <Modal
-            src={modalImg.src}
-            alt={modalImg.alt}
-            onClose={this.toggleModal}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={hendlerFormSubmit} />
+      <ImageGallery searchName={searchName} openModal={openModal} />
+      {shovModal && (
+        <Modal src={modalImg.src} alt={modalImg.alt} onClose={toggleModal} />
+      )}
+    </div>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.hendelKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.hendelKeyDown);
-  }
-
-  hendelKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  hendleClickBackdrop = e => {
+export const Modal = ({ src, alt, onClose }) => {
+  const hendleClickBackdrop = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={css.Overlay} onClick={this.hendleClickBackdrop}>
-        <div className={css.Modal}>
-          <img src={this.props.src} alt={this.props.alt} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  useEffect(() => {
+    const hendelKeyDown = e => {
+      console.log(e.code);
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', hendelKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', hendelKeyDown);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div className={css.Overlay} onClick={hendleClickBackdrop}>
+      <div className={css.Modal}>
+        <img src={src} alt={alt} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   src: PropTypes.string.isRequired,
